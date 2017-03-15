604757b466f9d6034fe137ca43a00c30ae1166aa
Skmsoumya <soumyakishormishra@gmail.com>
2017-03-15 12:22:31 +0530

    completed the new post form and its handler

--- main.py
+++ main.py
@@ -14,12 +14,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment( loader=jinja2.FileSystemLoader(template_dir),
+								autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kwargs):
+		self.response.write(*a, **kwargs)
+	def render_string(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+	def render(self, template, **params):
+		self.write(self.render_string(template, **params))
+
+class MainHandler(Handler):
+	def get(self):
+		self.render("homeTemplate.html")
+
+class AdminHandler(Handler):
+	def get(self):
+		self.render("adminPage.html")
+	def post(self):
+		errors = {}
+		errors["errors_present"] = False
+		blog_title = self.request.get("blogTitle")
+		blog_content = self.request.get("blogContent")
+		if not blog_title:
+			errors["title_error"] = "Please enter a valid Title!"
+			errors["errors_present"] = True
+		if not blog_content:
+			errors["content_error"] = "Please enter a Valid Content!"
+			errors["errors_present"] = True
+
+		if(errors["errors_present"]):
+			self.render("adminPage.html", title = blog_title, content = blog_content, errors = errors)
+		else:
+			self.render("post saved sucessfully")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ("/add_post", AdminHandler)
 ], debug=True)
